a877c826e74758d27b8b6d268edb254514fea2ec
Jesse Hixson <jesse.hixson@carney.co>
2018-08-17 16:05:04 -0400

    Updates sync task to match previous task. Adds profile option

--- lib/mix/tasks/images/sync.ex
+++ lib/mix/tasks/images/sync.ex
@@ -1,18 +1,30 @@
 defmodule Mix.Tasks.Images.Sync do
   use Mix.Task
 
-  def run([destination, bucket, _dryrun]) do
+  def run([destination, bucket, profile, _dryrun]) do
     IO.puts "Syncing with S3... (dry run)"
-    sync(destination, bucket, true)
+    sync(destination, bucket, profile, true)
   end
 
-  def run([destination, bucket]) do
+  def run([destination, bucket, profile \\ "collection"]) do
     IO.puts "Syncing with S3..."
-    sync(destination, bucket, false)
+    sync(destination, bucket, profile, false)
   end
 
-  defp sync(destination, bucket, dryrun) do
-    opts = ["s3", "sync", destination, "s3://#{bucket}", "--exclude=\"*\"", "--include=\"*.jpg\"", "--delete", "--acl=public-read"]
+  defp sync(destination, bucket, profile, dryrun) do
+    opts = [
+      "s3",
+      "sync",
+      destination,
+      "s3://#{bucket}",
+      "--exclude=\"*[1234567890]x[1234567890]*\"",
+      "--exclude=\"*DS_Store\"",
+      "--exclude=\"*thumb.jpg\"",
+      "--delete",
+      "--acl=public-read",
+      "--profile",
+      profile
+    ]
     sync = case dryrun do
       true -> opts ++ ["--dryrun"]
       _ -> opts
